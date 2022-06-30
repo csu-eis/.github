54976e65376232487fbad0599ee999af68da2bc4
SheepHuan <sheephuan@outlook.com>
2022-07-01 01:05:40 +0800

    20220701: 更新

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 
 
 
-def gen_master(members):
+def gen_table(members):
     codes=[]
     codes.append("<table>")
     codes.append("<tr>")
@@ -9,29 +9,20 @@ def gen_master(members):
         if i%5==0 and i!=0:
             codes.append("</tr>")
             codes.append("<tr>")
+        # 配置主页
+        page_url= f"https://github.com/{member[0]}"
+        if member[6]!="null":
+            page_url = member[6]
 
-        codes.append(f'<td align="center"><a href="https://github.com/{member[0]}"><img src="https://avatars.githubusercontent.com/u/{member[4]}?v=4?s=120" width="150px;" alt=""/><br /><sub><b>{member[2]}({member[3]})</b></sub></a><br /></td>')
+        codes.append(f'<td align="center"><a href="{page_url}"><img src="https://avatars.githubusercontent.com/u/{member[4]}?v=4?s=120" width="150px;" alt=""/><br /><sub><b>{member[2]}({member[3]})</b></sub></a><br /></td>')
     codes.append("</tr>")
     codes.append("</table>")
     return "\n".join(codes)
 
-def gen_phd(members):
-    codes=[]
-    codes.append("<table>")
-    codes.append("<tr>")
-    for i,member in enumerate(members):
-        if i%5==0 and i!=0:
-            codes.append("</tr>")
-            codes.append("<tr>")
-        codes.append(f'<td align="center"><a href="https://github.com/{member[0]}"><img src="https://avatars.githubusercontent.com/u/{member[4]}?v=4?s=120" width="150px;" alt=""/><br /><sub><b>{member[2]}({member[3]})</b></sub></a><br /></td>')
-
-    codes.append("</tr>")
-    codes.append("</table>")
-    return "\n".join(codes)
 
 members = open("csu-eis_members.csv").readlines()
 
-print(members)
+# print(members)
 phds=[]
 masters=[]
 mentors=[]
@@ -44,10 +35,10 @@ for member in members[1:]:
         masters.append(items)
     elif items[1] == 'mentor':
         mentors.append(items)
-print(phds)
+# print(phds)
 phds.sort(key=lambda x:int(x[5]))
 masters.sort(key=lambda x:int(x[5]))
 
-print(gen_phd(phds))
+print(gen_table(phds))
 print()
-print(gen_master(masters))
+print(gen_table(masters))
